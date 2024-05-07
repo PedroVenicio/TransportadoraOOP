2ef9c12461f32789b32a04ec10624b25731a8238
Eduardo.06 <eduardosamuelbarbosademoraes@gmail.com>
2024-05-07 17:17:58 -0300

    ajeitando a home

--- front-end/Screens/Home_tela.js
+++ front-end/Screens/Home_tela.js
@@ -1,31 +1,47 @@
-import {View, Text, TouchableOpacity} from 'react-native';
-import { useState } from 'react';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
-export default function Home({navigation}) {
-
-const [user, setUser] = useState(0);
-
-function usermais(){
-  setUser(prev => prev + 1);
-
-}
-function usermenos(){
-  setUser(prev => prev - 1);
-}
-
+export default function Home({ navigation }) {
   return (
-    <View >
-      <View>
-        <Text> Votação específica do usuário </Text>
-        <Text>{user}</Text>
-        <TouchableOpacity onPress={usermais}>
-        <Text>mais</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={usermenos}>
-        <Text>menos</Text>
-        </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <View style={[styles.squareOne, styles.red]}></View>
+        <View style={[styles.squareOne, styles.white]}></View>
       </View>
-
+      <View style={styles.row}>
+        <View style={[styles.squareOne, styles.white]}></View>
+        <View style={[styles.squareOne, styles.gray]}></View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  squareOne: {
+    width: "40%", // Defina o tamanho padrão dos quadrados
+    aspectRatio: 0.7, // Mantém a proporção quadrada
+    borderRadius: 15,
+    borderWidth: 2,
+    marginHorizontal: 5,
+  },
+  red: {
+    backgroundColor: 'red',
+  },
+  white: {
+    backgroundColor: 'white',
+  },
+  gray: {
+    backgroundColor: 'gray',
+  },
+});
