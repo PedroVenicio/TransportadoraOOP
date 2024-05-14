5f210255087586a62b61db91c106337c1fac0145
Eduardo.06 <eduardosamuelbarbosademoraes@gmail.com>
2024-05-14 17:26:36 -0300

    tela de login completo

--- front-end/Screens/Login_tela.js
+++ front-end/Screens/Login_tela.js
@@ -1,105 +1,126 @@
-import { TextInput, StyleSheet, Image, TouchableOpacity, Text, View} from 'react-native';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { TextInput, StyleSheet, Image, TouchableOpacity, Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
+export default function Login({ navigation }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-export default function Login({navigation}) {
-
-const [username,setUsername] =useState('');
-const [password,setPassword] =useState('');
-
-function Submit(){
-  if (username === 'dudu' && password === '1234'){
-  navigation.navigate('Home')
-
-  setUsername('');
-  setPassword('');
-} else{
-  alert('Dados inválidos')
-}}
+  function Submit() {
+    if (username === 'dudu' && password === '1234') {
+      navigation.navigate('Home');
+      setUsername('');
+      setPassword('');
+    } else {
+      alert('Dados inválidos');
+    }
+  }
 
   return (
-    <View style = {styles.container} >
-        <Text style = {styles.txt1}> Faça seu login em Ara Vans! </Text>
-        <View style={styles.img}>
-            <Image 
-            style={styles.imagem}
-            source={require('../ft/logo.png')} 
-            />      
-        </View>
-        <TextInput style = {styles.input} 
-            placeholder = "Usarname: "
-            placeholderTextColor={"white"}
+    <View style={styles.container}>
+      <View style={styles.img}>
+        <Image
+          style={styles.imagem}
+          source={require('../ft/MAVERIK.png')}
+        />
+      </View>
+      <Text style={styles.txt}>Faça seu login na</Text>
+      <Text style={styles.txt1}>transportadora Maverik!</Text>
+      <View style={styles.form}>
+        <View style={styles.inputContainer}>
+        <Feather name="user" size={24} color="gray" />
+          <TextInput
+            style={styles.input}
+            placeholder="Username: "
+            placeholderTextColor="gray"
             value={username}
-            onChangeText={setUsername}/>
-        <TextInput style = {styles.input} 
-            placeholder = "Senha:       "
-            placeholderTextColor={"white"} 
-            value={password}
-            onChangeText={setPassword}/>
-        <View style={styles.botom}>
-            <TouchableOpacity style={styles.container} onPress={Submit}>
-            <Text style = {styles.txtbotom} > login </Text>
-            </TouchableOpacity>
+            onChangeText={setUsername}
+          />
         </View>
-        <Text style = {styles.txt}> esqueceu a senha? </Text>
+        <View style={styles.inputContainer}>
+          <FontAwesome name="lock" size={24} color="gray" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Senha: "
+            placeholderTextColor="gray"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+        </View>
+      </View>
+      <View>
+        <TouchableOpacity style={styles.botom} onPress={Submit}>
+          <Text style={styles.txtbotom}>login</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.errosenha}>esqueceu a senha?</Text>
     </View>
   );
 }
 
-
-const styles = StyleSheet.create({ 
-  container:{
-    justifyContent:'center',
-    alignItems:'center',
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
     flex: 1,
-    paddingBottom: -5,
+    padding: 10,
+    backgroundColor: 'white',
   },
-
-  input: {
-    backgroundColor:'red',
-    paddingHorizontal:'36%',
-    display:'flex',
-    borderRadius: 15,
-    borderWidth: 2,
-    borderColor: 'black',
-    paddingBottom: 5,
-    paddingTop:5,
-    marginTop:5,
-    marginBottom:5,
-
-  },
-  botom: {
-    width: "50%",
-    height: "7%",
-    marginTop:10,
-    marginBottom:10,
-
-   
-  },
-  txtbotom: {
-    fontSize: 20,
-    color:"black",
-    alignItems: "center",
-    borderRadius: 8,
-    borderWidth: 2,
-    borderColor: 'black',
-  },
-   img:{
-    justifyContent:'center',
-    alignItems: 'flex-start',
-  },
-
   imagem: {
     height: 160,
     width: 160,
-    
+    marginBottom: 10,
   },
-    txt:{
-    fontSize: 15,
-    color:"red",
-  },
-  txt1:{
+  txt: {
     fontSize: 30,
-    color:"Black",
-   }
+    color: 'black',
+    fontStyle: 'italic',
+  },
+  txt1: {
+    fontSize: 30,
+    color: 'black',
+    fontStyle: 'italic',
+    marginBottom: 35,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    marginVertical: 5,
+    width: '70%',
+    height: 45,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+  },
+  botom: {
+    width: 100,
+    height: 30,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    borderWidth: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  txtbotom: {
+    fontSize: 20,
+    color: 'gray',
+  },
+  errosenha: {
+    marginTop: 10,
+    fontSize: 15,
+    color: 'red',
+  },
+  form: {
+    width: '100%',
+    alignItems: 'center',
+  },
 });
