a8b5333c7c6e8d7cbe293390f90647a694ea7b9a
EduardoSBM <eduardosamuelbarbosademoraes@gmail.com>
2024-06-10 21:28:13 -0300

    .

--- Screens/Demanda.js
+++ Screens/Demanda.js
@@ -1,59 +1,126 @@
 import React, { useState } from 'react';
 import { TextInput, StyleSheet, Image, TouchableOpacity, Text, View, ScrollView } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import { Feather, Fontisto, MaterialIcons, FontAwesome5, Entypo } from '@expo/vector-icons';
 
 export default function Demanda({ navigation }) {
   const [remetente, setRemetente] = useState('');
   const [enderecoRemetente, setEnderecoRemetente] = useState('');
-  const [enderecoDestinatario, setEnderecoDestinatario] = useState('');
-  const [destinatario, setDestinatario] = useState('');
-  const [valor, setValor] = useState('');
+  const [valorCargaSegurada, setValorCargaSegurada] = useState('');
   const [carga, setCarga] = useState('');
   const [pesoCarga, setPesoCarga] = useState('');
+  const [volume, setVolume] = useState('');
+  const [destinatario, setDestinatario] = useState('');
+  const [enderecoDestinatario, setEnderecoDestinatario] = useState('');
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.topo}>
-          <View style={styles.viewBox}>
-            <Image
-              style={styles.box}
-              source={require('../ft/box.png')}>
-            </Image>
-          </View>
-          <View style={styles.viewTxt1}>
-            <Text style={styles.txt1}> Crie sua demanda!</Text>
-          </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.topo}>
+        <View style={styles.viewBox}>
+          <Image
+            style={styles.box}
+            source={require('../ft/box.png')}>
+          </Image>
         </View>
-
-        <View style={styles.square}>
-          <View style={styles.inputContainer}>
-            <Feather name="remetente" size={24} color="red" />
-            <TextInput
-              style={styles.input}
-              placeholder="remetente: "
-              placeholderTextColor="gray"
-              value={remetente}
-              onChangeText={setRemetente}
-            />
-          </View>
-
-          <View>
-            <TouchableOpacity style={styles.botom} >
-              <Text style={styles.txtbotom}>login</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.viewTxt1}>
+          <Text style={styles.txt1}> Crie sua demanda!</Text>
         </View>
       </View>
-    </ScrollView>
+
+      <View style={styles.square}>
+        <View style={styles.inputContainer}>
+          <Feather name="user" size={24} color="red" />
+          <TextInput
+            style={styles.input}
+            placeholder="remetente: "
+            placeholderTextColor="gray"
+            value={remetente}
+            onChangeText={setRemetente}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Fontisto name="map-marker-alt" size={24} color="red" />
+          <TextInput
+            style={styles.input}
+            placeholder="Endereço do remetente: "
+            placeholderTextColor="gray"
+            value={enderecoRemetente}
+            onChangeText={setEnderecoRemetente}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <MaterialIcons name="attach-money" size={24} color="red" />
+          <TextInput
+            style={styles.input}
+            placeholder="Valor da carga segurada: "
+            placeholderTextColor="gray"
+            value={valorCargaSegurada}
+            onChangeText={setValorCargaSegurada}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Fontisto name="dropbox" size={24} color="red" />
+          <TextInput
+            style={styles.input}
+            placeholder="Carga : "
+            placeholderTextColor="gray"
+            value={carga}
+            onChangeText={setCarga}
+          />
+        </View>
+        <View style={styles.viewPV}>
+          <View style={styles.inputContainerPV}>
+            <FontAwesome5 name="weight-hanging" size={24} color="red" />
+            <TextInput
+              style={styles.input}
+              placeholder="Peso : "
+              placeholderTextColor="gray"
+              value={pesoCarga}
+              onChangeText={setPesoCarga}
+            />
+          </View>
+          <View style={styles.inputContainerPV}>
+            <Entypo name="resize-full-screen" size={24} color="red" />
+            <TextInput
+              style={styles.input}
+              placeholder="Volume : "
+              placeholderTextColor="gray"
+              value={volume}
+              onChangeText={setVolume}
+            />
+          </View>
+        </View>
+        <View style={styles.inputContainer}>
+        <Feather name="user" size={24} color="red" />
+          <TextInput
+            style={styles.input}
+            placeholder="Destinatário : "
+            placeholderTextColor="gray"
+            value={destinatario}
+            onChangeText={setDestinatario}
+          />
+        </View>
+        <View style={styles.minisquare}>
+
+        </View>
+          
+        <View>
+          <TouchableOpacity style={styles.botom} >
+            <Text style={styles.txtbotom}>login</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
+    //alignItems: 'center',
     flex: 1,
     backgroundColor: 'white',
+    //marginLeft: 15,
+    
+    
   },
   topo: {
     width: '100%',
@@ -85,8 +152,23 @@ const styles = StyleSheet.create({
   },
 
   square: {
-    width: "90%", // Defina o tamanho padrão dos quadrados
-    aspectRatio: 0.65, // Mantém a proporção quadrada
+    width: 350,
+    height: 900,
+    marginLeft: 20,
+    borderRadius: 15,
+    //marginHorizontal: 5,
+    alignItems: "center",
+    justifyContent: 'center',
+    backgroundColor: 'white', // Para garantir que a sombra seja visível
+    shadowColor: 'gray',  // Cor da sombra
+    shadowOffset: { width: 0, height: 4 },  // Deslocamento da sombra
+    shadowOpacity: 1.25,  // Opacidade da sombra
+    shadowRadius: 3.84,  // Raio da sombra
+    elevation: 5,  // Para Android, adiciona elevação
+  },
+  minisquare: {
+    width: '60%',
+    aspectRatio: 0.65,
     borderRadius: 15,
     marginHorizontal: 5,
     alignItems: "center",
@@ -97,17 +179,15 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,  // Raio da sombra
     elevation: 5,  // Para Android, adiciona elevação
   },
-
-
-
-
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     borderRadius: 25,
     paddingHorizontal: 10,
     marginVertical: 5,
-    width: '70%',
+    marginTop: 6,
+    marginBottom: 6,
+    width: '85%',
     height: 45,
     backgroundColor: 'white', // Para garantir que a sombra seja visível
     shadowColor: 'gray',  // Cor da sombra
@@ -115,6 +195,25 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,  // Opacidade da sombra
     shadowRadius: 3.84,  // Raio da sombra
     elevation: 5,  // Para Android, adiciona elevação
+  },
+  inputContainerPV: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 25,
+    paddingHorizontal: 10,
+    margin: 10,
+    marginVertical: 5,
+    width: '40%',
+    height: 45,
+    backgroundColor: 'white', // Para garantir que a sombra seja visível
+    shadowColor: 'gray',  // Cor da sombra
+    shadowOffset: { width: 0, height: 4 },  // Deslocamento da sombra
+    shadowOpacity: 0.25,  // Opacidade da sombra
+    shadowRadius: 3.84,  // Raio da sombra
+    elevation: 5,  // Para Android, adiciona elevação
+  },
+  viewPV: {
+    flexDirection: 'row',
   },
   icon: {
     marginRight: 10,
